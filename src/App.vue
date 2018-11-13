afd8320086231dcc0b18be4377e726b61b67e334
Josh Curtiss <git@curtiss.me>
2018-11-12 23:19:14 -0600

    Initial commit of new RPi app. For now it just shows the splash screen.

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id='app'>
+        <splash-screen msg='Welcome to Pi Guardian' />
+    </div>
+</template>
+
+<script>
+import SplashScreen from './components/SplashScreen.vue'
+
+export default {
+    name: 'app',
+    components: {
+        SplashScreen
+    }
+}
+</script>
+
+<style>
+body {
+    background-color:black;
+    margin: 0;
+}
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #eee;
+}
+</style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id='splash'>
+        <img alt="Pi Guardian logo" src="../assets/logo.png">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SplashScreen",
+    props: {
+        msg: String
+    }
+};
+</script>
+
+<style scoped>
+#splash {
+    text-align: center;
+}
+img {
+    height: 25vh;
+    margin-top: 35vh;
+}
+h1 {
+    color: #e89d31;
+    margin: 0;
+}
+</style>
