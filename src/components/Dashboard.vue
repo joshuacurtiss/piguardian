c031bdd22fe9678f662e4e84ef62a672c9fbe306
Josh Curtiss <git@curtiss.me>
2018-11-27 20:53:58 -0600

    Add multiple screen support to dashboard.
    - Supports two sets of screens, which are toggled by the toggle switch in upper-right corner.
    - Screens can swipe left/right like iOS screens.
    - Title displays name of the current screen.
    - Screens will be able to support three kinds of content, currently planned, not implemented yet:
      - Just any HTML content
      - A component which can render
      - An array will display a collection of widgets

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,25 +1,40 @@
 <template>
-    <transition name="fade">
+    <transition name="zoom">
         <div v-show='active' class='dashboard'>
             <header>
                 <div class='datetime'>
                     <dashboard-clock />
                 </div>
                 <div class='title'>
-                    {{ title }}
+                    {{ activeScreenKey }}
                 </div>
                 <div class='info'>
                     Organize by:
                     <toggle-button
-                        v-model='orgByRoomOrType'
+                        v-model='orgCustom'
                         :height='20'
-                        :width='64'
+                        :width='67'
                         :color='{checked: "#1970d4", unchecked: "#db8411", disabled: "#CCCCCC"}'
-                        :labels='{checked: "Room", unchecked: "Device"}'
+                        :labels='{checked: "Custom", unchecked: "Device"}'
                     />
                 </div>
             </header>
-            <main />
+            <main>
+                <transition name='fade'>
+                    <swipe v-show='!orgCustom' ref='deviceScreens' :auto='0' @change='handleChangeScreen'>
+                        <swipe-item v-for='i in activeDeviceScreenKeys' :key='i'>
+                            {{ activeDeviceScreens[i] }}
+                        </swipe-item>
+                    </swipe>
+                </transition>
+                <transition name='fade'>
+                    <swipe v-show='orgCustom' ref='customScreens' :auto='0' @change='handleChangeScreen'>
+                        <swipe-item v-for='i in activeCustomScreenKeys' :key='i'>
+                            {{ activeCustomScreens[i] }}
+                        </swipe-item>
+                    </swipe>
+                </transition>
+            </main>
             <footer>
                 <img class="logo" src="../assets/logo.png" />
                 <dashboard-event-bubble
@@ -47,6 +62,7 @@
 import DashboardClock from './DashboardClock.vue';
 import DashboardEventBubble from './DashboardEventBubble.vue';
 import Event from '../model/Event';
+import { Swipe, SwipeItem } from 'vue-swipe';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faCog, faTh, faPowerOff } from '@fortawesome/free-solid-svg-icons';
@@ -56,16 +72,45 @@ export default {
     components: {
         DashboardClock,
         DashboardEventBubble,
-        FontAwesomeIcon
+        FontAwesomeIcon,
+        Swipe,
+        SwipeItem
     },
     props: {
         active: Boolean
     },
     data () {
         return {
+            activeScreenIndex: 0,
+            customScreens: {
+                // TODO: Eventually remove this sample data
+                'Weather': 'My weather screen.',
+                'Living Room': [
+                    'blah', 'hello', 'another'
+                ],
+                'Dining Room': [
+                    'hiya', 'bye', 'bubbazinga'
+                ]
+            },
+            deviceScreens: {
+                // TODO: Eventually remove this sample data
+                'Lights/Switches': [
+                    'one', 'two', 'three'
+                ],
+                'Weather': 'My weather screen',
+                'Doors/Windows': [
+                    'Front Door', 'Back Door'
+                ],
+                'Locks': [
+                    'Front Deadbolt', 'Back Deadbolt'
+                ],
+                'Sensors': [],
+                'People': [
+                    'Josh', 'KD'
+                ]
+            },
             events: [],
-            orgByRoomOrType: false,
-            title: 'Starting up...'
+            orgCustom: false
         };
     },
     computed: {
@@ -74,18 +119,71 @@ export default {
         },
         activeEvents () {
             return this.events.filter(evt => evt.active);
+        },
+        activeScreensName () {
+            return this.orgCustom ? 'customScreens' : 'deviceScreens';
+        },
+        activeScreens () {
+            const screens = this[this.activeScreensName];
+            return this.filterActiveScreens(screens);
+        },
+        activeScreenKey () {
+            return this.activeScreenKeys[this.activeScreenIndex];
+        },
+        activeScreenKeys () {
+            return Object.keys(this.activeScreens);
+        },
+        activeCustomScreens () {
+            return this.filterActiveScreens(this.customScreens);
+        },
+        activeDeviceScreens () {
+            return this.filterActiveScreens(this.deviceScreens);
+        },
+        activeCustomScreenKeys () {
+            return Object.keys(this.activeCustomScreens);
+        },
+        activeDeviceScreenKeys () {
+            return Object.keys(this.activeDeviceScreens);
+        }
+    },
+    watch: {
+        orgCustom (newval) {
+            this.activeScreenIndex = 0;
+            this.$refs[this.activeScreensName].goto(0);
         }
     },
     methods: {
         addEvent (message) {
             this.events.push(new Event(message));
+        },
+        filterActiveScreens (screens) {
+            let obj = {};
+            Object.keys(screens).forEach(key => {
+                const item = screens[key];
+                // If it is an array but not empty, or some other truthy value... add it.
+                if ((Array.isArray(item) && item.length) || (Array.isArray(item) === false && item)) obj[key] = item;
+            });
+            return obj;
+        },
+        handleChangeScreen (index) {
+            this.activeScreenIndex = index;
         }
     },
     mounted () {
+        this.handleChangeScreen(0);
     }
 };
 </script>
 
+<style src='vue-swipe/dist/vue-swipe.css'></style>
+<style>
+/* Swipe Customizations */
+.mint-swipe-indicator {
+    background: #555;
+    opacity: 0.5;
+    margin: 0 0.75vw;
+}
+</style>
 <style scoped>
 .dashboard {
     height: 100vh;
@@ -116,8 +214,14 @@ header .title {
 header .info {
     text-align: right;
 }
+/* Main Content */
 main {
     margin: 1vh 1vw;
+}
+.mint-swipe {
+    position: absolute;
+    height: 84vh;
+    width: 98vw;
 }
 /* Control Board (where user controls stuff) */
 footer {
@@ -154,10 +258,20 @@ footer .btn:active {
     background-color: #888;
 }
 /* Transitions */
-.fade-enter-active, .fade-leave-active {
+.zoom-enter-active, .zoom-leave-active {
     transition: transform 0.3s ease 0.8s;
 }
+.zoom-enter, .zoom-leave-to {
+    transform: scale(1.5,1.5);
+}
+.fade-enter-active {
+    transition: all 0.3s ease 0.3s;
+}
+.fade-leave-active{
+    transition: all 0.3s ease;
+}
 .fade-enter, .fade-leave-to {
-    transform: scale(1.3,1.3);
+    opacity: 0;
+    transform: translateY(5vh);
 }
 </style>
