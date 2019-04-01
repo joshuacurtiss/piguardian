c7b1debee01b38900be4fb98ea9d09f1f3ddb74f
Josh Curtiss <git@curtiss.me>
2019-03-31 23:07:29 -0500

    Simplify SHM to have a separate icon component.
    For now, this simplifies the SHM code.  But soon we'll add ability to select the SHM settings, so will need to display multiple icons and perhaps move them around. So haven't a component that can gracefully resize and be a single encapsulated item makes things much easier.

---
+++ src/components/SmartHomeMonitorIcon.vue
@@ -0,0 +1,52 @@
+<template>
+    <font-awesome-layers fixed-width>
+        <font-awesome-icon icon='circle' />
+        <font-awesome-icon icon='home' transform='shrink-4' />
+        <font-awesome-icon icon='square-full' transform='shrink-12 down-3.25' />
+        <font-awesome-icon icon='circle-notch' spin v-show='loading' transform='shrink-11 down-2' />
+        <font-awesome-icon :icon='shmIcon' v-show='!loading' class='shmicon' transform='shrink-10.5 down-1.9' />
+    </font-awesome-layers>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
+import { faCircle, faCircleNotch, faHome, faLock, faQuestion, faSquareFull, faUnlock, faUser } from '@fortawesome/free-solid-svg-icons';
+library.add(faCircle, faCircleNotch, faHome, faLock, faQuestion, faSquareFull, faUnlock, faUser);
+export default {
+    name: 'smart-home-monitor-icon',
+    components: {
+        FontAwesomeIcon,
+        FontAwesomeLayers
+    },
+    props: {
+        'loading': {
+            type: Boolean,
+            default: false
+        },
+        'mode': {
+            required: true
+        }
+    },
+    computed: {
+        shmIcon () {
+            if (this.mode === 'off') return ['fas', 'unlock'];
+            else if (this.mode === 'away') return ['fas', 'lock'];
+            else if (this.mode === 'stay') return ['fas', 'user'];
+            else return ['fas', 'question'];
+        }
+    }
+};
+</script>
+
+<style scoped>
+svg[data-icon="home"], svg[data-icon="square-full"] {
+    color: #fafafa;
+}
+svg[data-icon="circle"], .shmicon {
+    color: #f3961c;
+}
+svg[data-icon="circle-notch"] {
+    color: #777;
+}
+</style>
--- src/components/screens/SmartHomeMonitorScreen.vue
+++ src/components/screens/SmartHomeMonitorScreen.vue
@@ -1,20 +1,10 @@
 <template>
     <div class="shmContainer">
-        <div class='iconContainer'>
-            <font-awesome-layers>
-                <font-awesome-icon
-                    icon='home'
-                />
-                <font-awesome-icon
-                    icon='square-full'
-                    class='doorfiller'
-                />
-                <font-awesome-icon
-                    :icon='shmIcon'
-                    class='shmicon'
-                />
-            </font-awesome-layers>
-        </div>
+        <smart-home-monitor-icon
+            :mode='shm'
+            :loading='loading'
+            class='shmIcon'
+        />
         <p>
             {{ shmDescription }}
         </p>
@@ -23,19 +13,16 @@
 
 <script>
 import electron from 'electron';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
-import { faHome, faLock, faQuestion, faSquareFull, faUnlock, faUser } from '@fortawesome/free-solid-svg-icons';
-library.add(faHome, faLock, faQuestion, faSquareFull, faUnlock, faUser);
+import SmartHomeMonitorIcon from '../SmartHomeMonitorIcon.vue';
 export default {
     name: 'smart-home-monitor-screen',
     components: {
-        FontAwesomeIcon,
-        FontAwesomeLayers
+        SmartHomeMonitorIcon
     },
     data () {
         return {
-            shm: null
+            shm: null,
+            loading: false
         };
     },
     computed: {
@@ -44,17 +31,12 @@ export default {
             else if (this.shm === 'away') return 'Armed (Away)';
             else if (this.shm === 'stay') return 'Armed (Stay)';
             else return 'Loading...';
-        },
-        shmIcon () {
-            if (this.shm === 'off') return ['fas', 'unlock'];
-            else if (this.shm === 'away') return ['fas', 'lock'];
-            else if (this.shm === 'stay') return ['fas', 'user'];
-            else return ['fas', 'question'];
         }
     },
     methods: {
         load () {
             console.log('Loading SHM status...');
+            this.loading = true;
             this.$http.get(window.settings.smartthings.uri + '/shm', {
                 headers: {
                     'Authorization': 'Bearer ' + window.settings.smartthings.token
@@ -63,6 +45,8 @@ export default {
                 if (response.body) this.shm = response.body.value;
             }).catch((exception) => {
                 console.error(exception);
+            }).finally(() => {
+                this.loading = false;
             });
         }
     },
@@ -78,30 +62,16 @@ export default {
 </script>
 
 <style scoped>
-.iconContainer {
-    width: 30vw;
-    height: 50vh;
-    background-color: #f3961c;
-    color: #fafafa;
-    border-radius: 50%;
-    margin-left: 32.5vw;
+.shmContainer {
     margin-top: 6vh;
-    font-size: 40vh;
-    padding: 3vh 0 0 3vw;
+    text-align: center;
 }
-.doorfiller {
-    font-size: 13vh;
-    margin: 24.5vh 0 0 10vw !important;
-}
-.shmicon {
-    color: #f3961c !important;
-    font-size: 18vh;
-    margin: 17vh 0 0 8.9vw !important;
+.shmIcon {
+    font-size: 56vh;
 }
 p {
-    text-align: center;
     font-size: 6vh;
-    margin-top: 5vh;
+    margin-top: 3vh;
     font-weight: bold;
 }
 </style>
